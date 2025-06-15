562f4a03ef227065b48e7a108581cebe92e56110
Alan Varghese <alanvz2212@example.com>
2025-06-15 20:39:04 +0530

    Updated

--- class.dart
+++ class.dart
@@ -1,1 +1,19 @@
+class Car {
+  // Properties (variables)
+  String brand;
+  int year;
 
+  // Constructor
+  Car(this.brand, this.year);
+
+  // Method (function)
+  void displayInfo() {
+    print("Brand: $brand, Year: $year");
+  }
+}
+
+void main() {
+  // Creating an object of Car
+  Car myCar = Car("Tesla", 2023);
+  myCar.displayInfo();  // Output: Brand: Tesla, Year: 2023
+}
